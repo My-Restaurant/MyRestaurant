b07758286d9359ad734c933c9ff3167249cbb3c4
Shray Tyagi <st.shray200130@gmail.com>
2021-06-10 22:30:24 +0530

    Login Side facebook

--- app/src/main/java/com/example/androidmyrestaurant/MainActivity.java
+++ app/src/main/java/com/example/androidmyrestaurant/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.androidmyrestaurant;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.btn_sign_in)
+    Button btn_sign_in;
+
+    @OnClick(R.id.btn_sign_in)
+    void loginUser()
+    {
+//        Intent intent = new Intent(this, AccountKitAcrivity.class);
+//        Facebook code to be deleted
+//            .
+//            .
+//             .
+//             .
+//             .
+//           .
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+//        Facebook code of login success
+//                and data entry to be deleted
+//
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
     }
 }
